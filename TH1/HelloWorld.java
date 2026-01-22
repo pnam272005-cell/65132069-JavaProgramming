657de698cd845417c52fa554cd8b76ffb1bf03bc
pnam272005-cell <pnam272005@gmail.com>
2026-01-23 06:26:51 +0700

    Add files via upload

---
+++ TH1/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Xin chào, tôi là Nguyễn Phương Nam!");
+        System.out.println("Đây là bài thực hành đầu tiên môn Lập trình thiết bị di động.");
+    }
+}
