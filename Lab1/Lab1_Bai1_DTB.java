9e56d630a89e896cb23f557430fd5f7a0e1c04fd
pnam272005-cell <pnam272005@gmail.com>
2026-02-01 11:57:27 +0700

    Add files via upload

---
+++ Lab1/Lab1_Bai1_DTB.java
@@ -0,0 +1,17 @@
+package Lab1;
+
+import java.util.Scanner;
+
+public class Lab1_Bai1_DTB {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Họ và tên sinh viên: ");
+        String hoTen = scanner.nextLine();
+        
+        System.out.print("Điểm trung bình: ");
+        double diemTB = scanner.nextDouble();
+        
+        System.out.printf("%s: %.2f điểm", hoTen, diemTB);
+    }
+}
---
+++ Lab1/Lab1_Bai4_Delta.java
@@ -0,0 +1,21 @@
+package Lab1;
+
+import java.util.Scanner;
+
+public class Lab1_Bai4_Delta {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Nhập hệ số a: ");
+        double a = scanner.nextDouble();
+        System.out.print("Nhập hệ số b: ");
+        double b = scanner.nextDouble();
+        System.out.print("Nhập hệ số c: ");
+        double c = scanner.nextDouble();
+        
+        double delta = Math.pow(b, 2) - 4 * a * c;
+        double canDelta = Math.sqrt(delta);
+        
+        System.out.printf("Delta: %.2f | Căn Delta: %.2f", delta, canDelta);
+    }
+}
